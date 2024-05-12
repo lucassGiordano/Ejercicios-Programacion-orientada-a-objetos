2b7cc924c25da9ca91ddb1489ee9cbae61068578
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-05-12 12:21:16 -0300

    Update PPP.c
    solo dios y yo saben que pasó acá

--- PPP.c
+++ PPP.c
@@ -25,37 +25,64 @@ int main(){
             scanf("%d",&e);
             em=(rand()%3)+1;
             int r=ppp(e,em);
-            printf("Maquina:%d\n",em);
+            printf("Maquina:%d\n\n",em);
             switch (r){
                 case 1:{
                     wins[i][j]=1;
-                    printf("ganaste\n");
+                    printf("ganaste\n\n");
                     break;
                 }
                 case 2:{
                     wins[i][j]=2;
-                    printf("perdiste\n");
+                    printf("perdiste\n\n");
                     break;
                 }
                 case 0:{
-                    wins[i][j]=3;
-                    printf("empate\n");
+                    wins[i][j]=0;
+                    printf("empate\n\n");
                     break;
                 }
             }
-            if(wins[i][1]==wins[i][2]){
-                wins[i][3]=wins[i][1];
-                break;
-            }
+            if(wins[i][0]==1 && wins[i][1] == 1){
+                wins[i][3]=1;
+                j=3;
+            };
+            if(wins[i][0]==2 && wins[i][1] == 2){
+                wins[i][3]=2;
+                j=3;
+            };
         }
-        wins[i][4]=wins[i][3];
-        if(wins[i][4]==1){ju+=1;}
-        else if(wins[i][4]==2){m+=1;}
-        else emp+=1;
-
-        if(ju>=3||m>=3){
-            break;
+        //verifica si es empate (siempre da 0) y descarta que el primero de 1 por el caso 111
+       /* int total= wins[i][0] + wins[i][1] + wins[i][2] ;
+       	if(total == 3 && wins[i][0] != 1){
+			wins[i][3]=0;
+		} else if(total == 2 && wins[i][0] != 1 || wins[i][1] != 1){
+			wins[i][3]=2;
+		} else if(total >= 4){
+			wins[i][3]=2;
+		} else if(total == 2 && (wins[i][0] == 1 || wins[i][1] == 1)){
+			wins[i][3]=1;
+		} else if(total == 1){
+			wins[i][3]=1;
+		}*/
+		int pm=0,pj=0;
+		for(j=0; j<4; j++){
+        	if(wins[i][j]==1){
+			pj++;
+			} else if(wins[i][j]==2){
+			pm++;
+			}
         }
+        if(pm==pj){
+         wins[i][3]= 0;
+         emp++;
+		} else if(pm>pj){
+			wins[i][3]= 2;
+         	m++;
+		} else if(pm<pj){
+			wins[i][3]= 1;
+         	ju++;
+		}
     }
 
     if(ju>m){
@@ -64,5 +91,10 @@ int main(){
         printf("perdiste la partida\n");
     }
     printf("ganadas:%d perdidas:%d empate:%d\n",ju,m,emp);
+    for(i=0; i<5; i++){
+        for(j=0; j<4; j++){
+        	printf("%d ",wins[i][j]);
+        }
+        printf("\n");
+    }
 }
-
